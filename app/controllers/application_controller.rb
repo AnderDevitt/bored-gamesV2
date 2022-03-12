8f98509af90b40e7920931a1dd38139c407da90a
andrew <gcab022116@coderacademy.edu.au>
2022-03-12 15:31:47 +1000

    this commit has address correctly linked to user!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, address_attributes: [:street_number, :street_name, :suburb, :state, :postcode]])
+    end
 end
+
+# this will help later when I add the address
+    # before_action :configure_permitted_parameters, if: :devise_controller?
+
+    #   protected
+
+    #   def configure_permitted_parameters
+    #     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, address_attributes: [:country, :state, :city, :area, :postal_code]])
+    #   end
+
