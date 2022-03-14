d256dbce021d8780b075a1d11d8d8eeaa1df9273
andrew <gcab022116@coderacademy.edu.au>
2022-03-14 13:57:49 +1000

    address show issue finally fixed; dropdown menu item added to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,22 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Send back the home page
   get "pages/index"
   root to: "pages#index"
   
-
+  # Send back all addresses to the client for Admin use
   get 'addresses', to: 'addresses#index'
+  #
   post 'addresses', to: 'addresses#create'
   get 'addresses/new', to: 'addresses#new'
+  # Send back one address to the client
   get 'addresses/:id', to: 'addresses#show', as: 'address'
+  #edit an address
   get 'addresses/:id/edit', to: 'addresses#edit', as: "edit_address"
   patch 'addresses/:id', to: 'addresses#update'
   put 'addresses/:id', to: 'addresses#update'
-  delete 'addresses/:id', to: 'addresses#destroy'
+  
 
   get 'games', to: 'games#index'
   post 'games', to: 'games#create'
